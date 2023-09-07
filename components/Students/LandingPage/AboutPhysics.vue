00c6bc3b323cec1b752d0bf38e75112e34b21c8b
zeyadMoamen <zeyad.moamen@hamza.solutions>
2023-09-07 09:59:23 +0300

    Last updates

--- components/Students/LandingPage/AboutPhysics.vue
+++ components/Students/LandingPage/AboutPhysics.vue
@@ -2,11 +2,11 @@
   <div>
     <h6 class="about-physics__title">عن الفيزياء</h6>
 
-    <div>
+    <div class="about-physics__img">
       <img
         role="button"
         class="w-100"
-        src="@/assets/imgs/landing-page/about-physics.png"
+        src="@/assets/imgs/landing-page/3671320.jpg"
         alt=""
       />
     </div>
